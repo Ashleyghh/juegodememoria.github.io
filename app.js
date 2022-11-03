2e6d38a7b41f22e841ba2bff48578c3adfa36e25
Ashleyghh <107528373+Ashleyghh@users.noreply.github.com>
2022-11-03 17:54:52 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,125 @@
+const cards = document.querySelectorAll(".memory-card");
+const refresh = document.querySelector(".refresh img");
+const final = document.querySelector(".final");
+const congrats = document.querySelector("#congratsSection");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+const again = document.querySelector(".again");
+const totalTime = document.querySelector("#totalTime");
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false; //ard arda 4 tıklamaya izin vermemek icin
+let totalSeconds = 0;
+let interval;
+let finalTime;
+let click = -1;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+    startTime();
+    return;
+  }
+  //second click
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.id === secondCard.dataset.id;
+  isMatch ? disableCards() : unFlipCards();
+}
+
+function disableCards() {
+  //eşleştiğinde
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  resetBoard();
+  gameWon();
+}
+
+function unFlipCards() {
+  lockBoard = true;
+  //not match
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    resetBoard();
+  }, 700);
+}
+
+function resetBoard() {
+  // card döndüğünde tekrar tıklayabilmemizi sağlar
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+//refresh butonuna basildiginda
+refresh.addEventListener("click", function () {
+  confirm("Estás segur@ de volver a reiniciar el juego?");
+  location.reload();
+});
+
+//zaman göstergesi
+function startTime() {
+  if (click === -1) {
+    interval = setInterval(function () {
+      //interval belirli araliklarla sürekli çalişmasini saglar.
+      final.innerHTML = "Felicitaciones. acabaste de ganar en un tiempo de: " + finalTime + "!";
+      finalTime = minute.innerHTML + ":" + second.innerHTML;
+      totalSeconds++;
+      second.innerHTML = pad(totalSeconds % 60);
+      minute.innerHTML = pad(parseInt(totalSeconds / 60));
+    }, 1000);
+  }
+  click = 1;
+}
+
+function pad(val) {
+  // 00:00 şeklinde ilerlesin zaman.
+  const valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+
+//game won
+function gameWon() {
+  if (click < 1) {
+    // clickler saniyenin durması için. Oyun bitse dahi saniye devam etmesini engeller.
+    firstCard = e.target;
+  }
+
+  if (document.getElementsByClassName("flip").length === 12) {
+    congratsSection.classList.replace("hidden", "show");
+    clearInterval(interval);
+    finalTime = minute.innerHTML + ":" + second.innerHTML;
+    final.innerHTML = "Felicitaciones. acabaste de ganar en un tiempo de: " + finalTime + "!";
+    totalTime.innerHTML = finalTime;
+  }
+  click = 0;
+}
+
+//congrats section'da again butonu
+again.addEventListener("click", function () {
+  congratsSection.classList.replace("show", "hidden");
+  location.reload();
+});
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
